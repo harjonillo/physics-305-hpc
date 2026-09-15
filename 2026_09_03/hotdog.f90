02069437d174a8245158a7988b6311f70a83789d
Hannah Arjonillo <83525082+harjonillo@users.noreply.github.com>
2026-09-15 17:28:39 +0800

    Add parallelization

--- 2026_09_03/hotdog.f90
+++ 2026_09_03/hotdog.f90
@@ -17,9 +17,31 @@ subroutine waffle(x, a, N)
     do i = 1, N
         do j = 1, i-1
             a(i) = a(i) + (x(i) - x(j))**(-2)
+            a(j) = a(j) + (x(i) - x(j))**(-2)
         end do
         do j = i+1, N
             a(i) = a(i) + (x(i) - x(j))**(-2)
+            a(j) = a(j) + (x(i) - x(j))**(-2)
         end do
     end do
+end subroutine
+
+
+subroutine waffle2(x, a, N)
+    implicit none
+    real(8), intent(in) :: x(N)
+    real(8), intent(out) :: a(N)
+    integer(8), intent(in) :: N
+    real(8) :: atmp  ! temporary variable to reduce the number of times we compute the sq term
+    integer(8) :: i, j
+    a = 0.0
+    !$OMP PARALLEL DO PRIVATE(atmp) SCHEDULE(DYNAMIC, 10)
+    do i = 1, N
+        do j = i+1, N
+            atmp = (x(i) - x(j))**(-2)
+            a(i) = a(i) + atmp
+            a(j) = a(j) + atmp
+        end do
+    end do
+    !$OMP END PARALLEL DO
 end subroutine
